e4afb9acb5820d8b7c7a2917278756edf1f8eda6
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-08-21 21:44:08 -0300

    Aula 05 Atividade 02: Verificando compatibilidade de tipos

--- src/br/com/alura/alurator/Alurator.java
+++ src/br/com/alura/alurator/Alurator.java
@@ -50,4 +50,9 @@ public class Alurator {
 		
 		return retorno;
 	}
+
+	public void registra(Class<?> tipoFonte, Class<?> tipoDestino) {
+		container.registra(tipoFonte, tipoDestino);
+	}
+	
 }
--- src/br/com/alura/alurator/ioc/ContainerIoC.java
+++ src/br/com/alura/alurator/ioc/ContainerIoC.java
@@ -4,13 +4,22 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 public class ContainerIoC {
 
+	private Map<Class<?>, Class<?>> mapaDeTipos = new HashMap<>();
+	
 	public Object getInstancia(Class<?> tipoFonte) {
+		Class<?> tipoDestino = mapaDeTipos.get(tipoFonte);
+		if (tipoDestino != null) {
+			return getInstancia(tipoDestino);
+		}
+		
 		Stream<Constructor<?>> construtores = 
 				Stream.of(tipoFonte.getDeclaredConstructors());
 		
@@ -38,6 +47,35 @@ public class ContainerIoC {
 				 | IllegalArgumentException | InvocationTargetException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	public void registra(Class<?> tipoFonte, Class<?> tipoDestino) {
+		boolean compativel = verificaCompatibilidade(tipoFonte, tipoDestino);
+		if (!compativel) {
+			throw new ClassCastException("Não foi possível resolver " + tipoFonte.getName() + " para " + tipoDestino.getName());
+		}
+		
+		mapaDeTipos.put(tipoFonte, tipoDestino);
+	}
+
+	private boolean verificaCompatibilidade(Class<?> tipoFonte, Class<?> tipoDestino) {
+		// verificando a compatibilidade na raça		
+//		boolean compativel;
+//		
+//		if (tipoFonte.isInterface()) {
+//			compativel = Stream
+//							.of(tipoDestino.getInterfaces())
+//							.anyMatch(interfaceImplementadas -> interfaceImplementadas.equals(tipoFonte));
+//		} else {
+//			compativel =
+//					tipoDestino.getSuperclass().equals(tipoFonte)
+//					|| tipoDestino.equals(tipoFonte);
+//		}
+//		
+//		return compativel;
+		
+		// verificando a compatibilidade com a API de Reflection
+		return tipoFonte.isAssignableFrom(tipoDestino);
 	}
 	
 }
